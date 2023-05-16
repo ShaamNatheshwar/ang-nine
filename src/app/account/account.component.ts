7c298898d1f69da5f04570af016b05698be847ac
ssn <shaamnatheshwar@gmail.com>
2023-05-16 14:59:58 +0530

    done till 5

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,21 +1,24 @@
 import { Component, Input, EventEmitter, Output } from '@angular/core';
 import { LoggingService } from '../logging.service';
+import { AccountService } from '../accounts.service';
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers: [LoggingService]
+  providers: [LoggingService, AccountService]
 
 })
 export class AccountComponent {
   @Input() account : {name : string, status : string};
   @Input() id : number;
   @Output() statusChanged = new EventEmitter<{id: number, newStatus: any}> 
-  constructor(private loggingService : LoggingService){}
+  constructor(private loggingService : LoggingService,
+    private accountsService: AccountService
+    ){}
 
 
   onSetTo(status: string){
-    this.statusChanged.emit({id: this.id, newStatus: status});
+    this.accountsService.updateStatus(this.id, status)
     this.loggingService.logStatusChange(status);
   }
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,17 @@
 import { Component } from '@angular/core';
+import { AccountService } from './accounts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AccountService]
 })
 export class AppComponent {
-  title = 'nine';
-  accounts = [
-    {
-      name : 'Master Account',
-      status : 'active'
-    },
-    {
-      name : 'Test account',
-      status : 'inactive'
-    },
-    {
-      name : 'Hidden Account',
-      status : 'unknown'
-    },
-  ];
+  accounts: {name: string, status : string}[] = [];
+  constructor(private accountsService: AccountService){}
 
-  onAccountAdded(newAccount: {name: string, status : string}){
-    this.accounts.push(newAccount)
-  }
-  onStatusChanged(updateInfo: {id: string, newStatus : string}){
-    this.accounts[updateInfo.id].status=updateInfo.newStatus
+  ngOnInit(){
+    this.accounts = this.accountsService.accounts; 
   }
 }
